f68500139bdaa9d15fe38774a9ecadfc981bc692
Aleksandar Stojiljkovic <aca.alex944@gmail.com>
2022-04-28 18:07:12 +0200

    domaci-2

---
+++ domaci_zadatak_2/script.js
@@ -0,0 +1,21 @@
+
+//1. Smisliti ideju za objekat i kreirati objekat sa razlicitim kljucevima
+
+//2. Objekat treba da ima string, number, funkciju
+
+let person = {
+
+  name: 'Aleksandar',
+  surname: 'Stojiljkovic',
+  age: 27,
+
+  
+  
+}
+function mnozenje(prviBroj, drugiBroj) {
+  let rezultat = prviBroj * drugiBroj;
+  
+  return rezultat;
+}
+
+mnozenje(10, 10);
